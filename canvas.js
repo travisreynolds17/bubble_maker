0a528473768a6bd2da3e9c1ebeccbff9b7f2b18f
Travis Reynolds <travisreynolds17@gmail.com>
2018-10-23 13:26:15 -0400

    clean up

--- canvas.js
+++ canvas.js
@@ -1,14 +1,15 @@
 let canvas = document.querySelector("canvas");
-let btnGrid = document.querySelector(".draw");
 let btnX = document.querySelector(".x");
 let btnY = document.querySelector(".y");
 let inputCircles = document.querySelector("#circles");
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+let color3 = document.querySelector("#color3");
 
 // this basically specifies that the variable references the previously selected canvas AS as a canvas, rather than just an HTML node. the argument 2d
 // tells us which methods are open to the canvas.
 let c = canvas.getContext("2d");
 
-btnGrid.addEventListener("click", drawGrid(100, 100, 6, 6, 40));
 btnX.addEventListener("click", clear);
 btnY.addEventListener("click", userCircles);
 
@@ -19,47 +20,35 @@ canvas.height = 0.6 * window.innerHeight;
 let circleArray = [];
 // this variable will be used to let us stop requesting animation frames
 let animating = false;
+// holds user selectable color values
+let colors = [
+  "black",
+  "blue",
+  "teal",
+  "red",
+  "yellow",
+  "green",
+  "navy",
+  "white",
+  "gray",
+  "orange"
+];
+let colorSelects = [color1, color2, color3];
+let userColors = []; // to hold user-selected color choices
 
-function drawGrid(x, y, rows, columns, tileSize) {
-  let width = columns * tileSize;
-  let height = rows * tileSize; 
+function init() {
+  populateSelects();
+}
+function populateSelects() {
+  let templateStart = "<option>";
+  let templateEnd = "</option>\n";
+  let tempColors = [];
 
-  let y2 = y;
-  let x2 = x;
-  posX = x;
-  posY = y;
-
-  c.beginPath();
-  // draw a square border
-  c.moveTo(x, y);
-  posX = x + width;
-  c.lineTo(posX, posY);
-  posY = y + height;
-  c.lineTo(posX, posY);
-  posX = x;
-  c.lineTo(posX, posY);
-  posY = y;
-  c.lineTo(posX, posY);
-  c.stroke();
-
-  // draw row lines
-  posX = x + width;
-  for (let i = 0; i < rows; i++) {
-    c.moveTo(x, y2);
-
-    c.lineTo(posX, y2);
-    c.stroke();
-    y2 += tileSize;
+  for (let index = 0; index < colors.length; index++) {
+    tempColors[index] = templateStart + colors[index] + templateEnd;
   }
-  //draw column lines
 
-  posY = y + height;
-  for (let i = 0; i < columns; i++) {
-    c.moveTo(x2, y);
-    c.lineTo(x2, posY);
-    c.stroke();
-    x2 += tileSize;
-  }
+  colorSelects.forEach(item => (item.innerHTML = tempColors));
 }
 
 function clear() {
@@ -68,6 +57,7 @@ function clear() {
   circleArray = [];
 }
 function userCircles() {
+  let fillColor = "";
   if (inputCircles.value <= 0 || inputCircles.value > 100) {
     alert("Please enter a positive value up to 100");
   } else {
@@ -82,44 +72,22 @@ function userCircles() {
       let y = Math.random() * (canvas.height - radius * 2) + radius;
       let yx = (Math.random() - 0.5) * 7;
 
-      circleArray[index] = new Circle(x, y, dx, yx, radius, "green", "black");
+      // set fill color based on user selections
+      let thirds = Math.floor(inputCircles.value) / 3;
+      if (index < thirds) {
+        fillColor = color1.value;
+      } else if (index > thirds * 2) {
+        fillColor = color3.value;
+      } else {
+        fillColor = color2.value;
+      }
+
+      // generate
+      circleArray[index] = new Circle(x, y, dx, yx, radius, "green", fillColor);
     }
     animate();
   }
 }
-
-function pattern(x, y, x2, y2, changeBy, times) {
-  var x3 = x,
-    y3 = y,
-    y4 = y2,
-    x4 = x2;
-  for (var i = 0; i <= times; i++) {
-    x3 += changeBy;
-    x4 += changeBy;
-    y3 += changeBy;
-    y4 += changeBy;
-    if (i == times) {
-      c.fillStyle = "gold";
-    }
-    c.fillRect(x3, y3, x4, y4);
-  }
-}
-
-function randomSquares(occurences) {
-  for (let i = 0; i <= occurences; i++) {
-    x = Math.random() * window.innerWidth;
-    y = Math.random() * window.innerHeight;
-    times = Math.random() * 30;
-    changeBy = Math.random() * 32;
-    times = Math.random() * 112;
-    pattern(x, y, 50, 50, changeBy, times);
-    console.log("hi");
-  }
-}
-
-//drawGrid(100, 100, 6, 6, 40);
-
-///////////////////////////////////// circle stuff
 
 function Circle(x, y, dx, yx, radius, color, fill) {
   this.x = x;
@@ -163,3 +131,5 @@ function animate() {
     circleArray.forEach(item => item.update());
   }
 }
+
+init();
